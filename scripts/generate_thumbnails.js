fdc21e502431a1d760202704af9ff5d114490719
HanaeRateau <hanaerateau@yahoo.fr>
2026-03-03 17:13:50 +0100

    Adds puppeteer

--- scripts/generate_thumbnails.js
+++ scripts/generate_thumbnails.js
@@ -1,4 +1,4 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const urls = JSON.parse(fs.readFileSync('data/projects.json', 'utf8'));
