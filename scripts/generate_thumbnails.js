c4ff545289ff37720898d9f2a93904af3f209954
HanaeRateau <hanaerateau@yahoo.fr>
2026-03-03 17:49:32 +0100

    slugify screenshots names instead of URI encoding

--- scripts/generate_thumbnails.js
+++ scripts/generate_thumbnails.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const { utils } = require('./utils');
 
 const urls = JSON.parse(fs.readFileSync('data/projects.json', 'utf8'));
 const noimage_urls = urls.filter((project)=>!project.image && project.demo);
@@ -7,11 +8,12 @@ const noimage_urls = urls.filter((project)=>!project.image && project.demo);
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 
-  for (const { title, url } of noimage_urls) {
-    const name = encodeURIComponent(title);
+  for (const { title, demo } of noimage_urls) {
+    console.log("Screenshoting ", demo);
+    const name = utils.slugify(title);
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 720 });
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    await page.goto(demo, { waitUntil: 'networkidle2' });
     await page.screenshot({ path: `thumbnails/${name}.png`, clip: { x: 0, y: 0, width: 1280, height: 720 } });
     console.log(`✓ ${name}`);
     await page.close();
